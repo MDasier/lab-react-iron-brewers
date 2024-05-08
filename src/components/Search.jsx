bac8e97c6847752650822ab7aeb21b06ccdc50d7
Asier MD <asier.md09@gmail.com>
2024-05-08 18:00:01 +0200

    done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,22 @@
-function Search() {
+import axios from "axios";
+import { useState,useEffect } from "react";
+function Search(props) {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    //const delayDebounceFn = setTimeout(() => {
+      axios.get(`${import.meta.env.VITE_BACKEND_URL}/search?q=${searchTerm}`)
+      .then((resp)=>{
+        props.setBeers(resp.data)
+       })
+       .catch((error)=>{
+        console.log(error)
+       })
+    //}, 1000)
+
+    //return () => clearTimeout(delayDebounceFn)
+  }, [searchTerm])
+
   return (
     <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
       <div className="input-group mb-2 w-50">
@@ -10,6 +28,7 @@ function Search() {
         <input
           type="text"
           className="form-control search-bar"
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
     </div>
